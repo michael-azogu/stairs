b52827820cb11ae585baccfaddf92c71541a95c1
michael-azogu <michaelazogu101@gmail.com>
2024-07-01 06:56:26 +0100

    stepping with no stairs

---
+++ src/main.ts
@@ -0,0 +1,353 @@
+import './style.css'
+import {
+  log,
+  clone,
+  part,
+  make,
+  fill_frames,
+  compute_end,
+  convert,
+} from './utils.ts'
+import type { Coord, Part } from './utils.ts'
+
+const main = document.createElement('canvas')
+main.width = 500
+main.height = 500
+
+const scene = main.getContext('2d')!
+const blank = scene.createImageData(main.width, main.height)
+
+document.querySelector<HTMLDivElement>('#app')!.append(main)
+
+const [ht, ht_ctx] = clone(main)
+const [left, left_ctx] = clone(main)
+const [right, right_ctx] = clone(main)
+
+const layers = [main, left, ht, right]
+
+function cycle<T>(list: Array<T>) {
+  function* _(list: Array<T>) {
+    while (true) {
+      for (const item of list) {
+        yield item
+      }
+    }
+  }
+  const $ = _(list)
+  return () => $.next().value as T
+}
+
+const waist /* FIXED POINT */ = part({
+  parent: null,
+  x: 200,
+  y: 250,
+  θ: 0,
+  magnitude: 0,
+})
+
+const base = 35
+
+const torso = part({
+  parent: waist,
+  magnitude: 2.5 * base,
+  θ: 67.5,
+})
+
+const head = part({
+  parent: torso,
+  magnitude: base,
+  θ: torso.θ, // tie to torso
+})
+
+const upper_arms = 1.3 * base
+const arm_right_upper = part({
+  parent: torso,
+  magnitude: upper_arms,
+  θ: 225,
+})
+const arm_left_upper = part({
+  parent: torso,
+  magnitude: upper_arms,
+  θ: 315,
+})
+
+const lower_arms = 1.15 * upper_arms
+const arm_right_lower = part({
+  parent: arm_right_upper,
+  magnitude: lower_arms,
+  θ: 300,
+})
+const arm_left_lower = part({
+  parent: arm_left_upper,
+  magnitude: lower_arms,
+  θ: 30,
+})
+
+const upper_legs = 1.8 * base
+const leg_right_upper = part({
+  parent: waist,
+  magnitude: upper_legs,
+  θ: 315,
+})
+const leg_left_upper = part({
+  parent: waist,
+  magnitude: upper_legs,
+  θ: 270,
+})
+
+const lower_legs = 0.9 * upper_legs
+const leg_right_lower = part({
+  parent: leg_right_upper,
+  magnitude: lower_legs,
+  θ: 270,
+})
+const leg_left_lower = part({
+  parent: leg_left_upper,
+  magnitude: lower_legs,
+  θ: 270,
+})
+
+const feet = 0.6 * base
+const foot_right = part({
+  parent: leg_right_lower,
+  magnitude: feet,
+  θ: 0,
+})
+const foot_left = part({
+  parent: leg_left_lower,
+  magnitude: feet,
+  θ: 0,
+})
+
+const arc =
+  2 * Math.PI -
+  Math.atan2(
+    position(foot_right).y - waist.y!,
+    position(foot_right).x - waist.x!
+  )
+
+const radius = Math.sqrt(
+  (position(foot_right).y - waist.y!) ** 2 +
+    (position(foot_right).x - waist.x!) ** 2
+)
+
+const stair = { θ : 0}
+const footcycle = cycle([foot_right, foot_left])
+function paint_stairs() {
+  let stairway = [
+    ...fill_frames(0.15, 180),
+    ...fill_frames(0, 180 * 3),
+  ]
+
+  redraw_layer(scene, (ctx) => {
+    let { x, y } = { x: waist.x!, y: waist.y! }
+    ctx.beginPath()
+    ctx.fillStyle = '#000'
+    ctx.fillRect(x, y, 60, 60)
+
+    ctx.fillStyle = '#000'
+    compute_end({ x, y }, stair.θ +  arc, radius)
+    ctx.fillRect(x1, y1, 60, 60)
+    
+    ctx.fillStyle = '#000'
+    compute_end({ x, y }, stair.θ +  arc, radius)
+    ctx.fillRect(x1, y1, 60, 60)
+    
+    ctx.fillStyle = '#000'
+    compute_end({ x, y }, stair.θ +  arc, radius)
+    ctx.fillRect(x1, y1, 60, 60)
+    ctx.closePath()
+  })
+}
+
+function paint_head_torso() {
+  redraw_layer(ht_ctx, (ctx) => {
+    head: {
+      let { x, y } = compute_end(
+        compute_end(position(torso), torso.θ, torso.magnitude),
+        torso.θ,
+        head.magnitude / 2
+      )
+      ctx.beginPath()
+      ctx.lineWidth = 5
+      ctx.arc(x, y, head.magnitude / 2, 0, 2 * Math.PI)
+      ctx.stroke()
+      ctx.closePath()
+
+      ctx.beginPath()
+      ctx.lineWidth = 2
+      ctx.arc(x + 7, y - 2, 2, 0, 2 * Math.PI)
+      ctx.stroke()
+      ctx.closePath()
+    }
+
+    torso: {
+      draw_appendage(torso, ctx)
+    }
+  })
+}
+
+function paint_right() {
+  redraw_layer(right_ctx, (ctx) => {
+    ;[
+      arm_right_upper,
+      arm_right_lower,
+      leg_right_upper,
+      leg_right_lower,
+      foot_right,
+    ].forEach((p) => draw_appendage(p, ctx))
+  })
+}
+
+function paint_left() {
+  redraw_layer(left_ctx, (ctx) => {
+    ;[
+      arm_left_upper,
+      arm_left_lower,
+      leg_left_upper,
+      leg_left_lower,
+      foot_left,
+    ].forEach((p) => draw_appendage(p, ctx))
+  })
+}
+
+function draw_appendage(part: Part, ctx: CanvasRenderingContext2D) {
+  const { x, y } = position(part)
+  const { x: x1, y: y1 } = compute_end({ x, y }, part.θ, part.magnitude)
+
+  ctx.beginPath()
+  ctx.lineWidth = 6
+  // ctx.strokeStyle =
+  // '#' +
+  // Math.floor(Math.random() * 0xffffff)
+  //   .toString(16)
+  //   .padStart(6, '0')
+  ctx.moveTo(x, y)
+  ctx.lineTo(x1, y1)
+  ctx.stroke()
+  ctx.closePath()
+}
+
+function paint() {
+  paint_right()
+  paint_head_torso()
+  paint_left()
+  paint_stairs()
+}
+
+paint()
+
+// Δθ
+const torso_frames = cycle([
+  ...fill_frames(22.5, 90 * 2),
+  ...fill_frames(-22.5, 45 * 2),
+])
+
+const swing_ua_forwards = [...fill_frames(90, 135 * 2)]
+const swing_ua_backwards = swing_ua_forwards
+  .slice()
+  .reverse()
+  .map((d) => -d)
+
+const ula_frames = cycle([...swing_ua_backwards, ...swing_ua_forwards])
+const ura_frames = cycle([...swing_ua_forwards, ...swing_ua_backwards])
+
+const swing_lower_forwards = [...fill_frames(60, 135 * 2)]
+const swing_lower_backwards = swing_lower_forwards
+  .slice()
+  .reverse()
+  .map((d) => -d)
+const lla_frames = cycle([...swing_lower_backwards, ...swing_lower_forwards])
+const lra_frames = cycle([...swing_lower_forwards, ...swing_lower_backwards])
+
+// LEGS
+const ull = [
+  ...fill_frames(0, 90),
+  ...fill_frames(45, 90),
+  ...fill_frames(0, 90),
+  ...fill_frames(-45, 90),
+  ...fill_frames(0, 180),
+]
+
+const ulr = [
+  ...fill_frames(-45, 90),
+  ...fill_frames(0, 270),
+  ...fill_frames(45, 90),
+  ...fill_frames(0, 90),
+]
+
+const lll = [
+  ...fill_frames(-45, 90),
+  ...fill_frames(0, 90),
+  ...fill_frames(45, 90),
+  ...fill_frames(0, 270),
+]
+
+const llr = [
+  ...fill_frames(0, 270),
+  ...fill_frames(-45, 90),
+  ...fill_frames(0, 90),
+  ...fill_frames(45, 90),
+]
+
+const ull_frames = cycle(ull)
+const ulr_frames = cycle(ulr)
+
+const lll_frames = cycle(lll)
+const llr_frames = cycle(llr)
+
+// ! ADD FEET TURNS
+setInterval(() => {
+  torso.θ += torso_frames()
+
+  arm_left_upper.θ += ula_frames()
+  arm_right_upper.θ += ura_frames()
+
+  arm_left_lower.θ += lla_frames()
+  arm_right_lower.θ += lra_frames()
+
+  leg_left_upper.θ += ull_frames()
+  leg_right_upper.θ += ulr_frames()
+
+  leg_left_lower.θ += lll_frames()
+  leg_right_lower.θ += llr_frames()
+
+  paint()
+}, 40)
+
+let is_holding = false
+
+document.addEventListener('keydown', (e) => {
+  if (e.key == 'ArrowRight') is_holding = true
+})
+
+document.addEventListener('keyup', (e) => {
+  if (e.key == 'ArrowRight') is_holding = false
+})
+
+function position(part: Part): { x: number; y: number } {
+  function query(p: Part): Coord {
+    if (p.parent == null) return { x: p.x!, y: p.y! }
+
+    const { x: sx, y: sy } = query(p.parent)
+
+    return compute_end({ x: sx, y: sy }, p.θ, p.magnitude)
+  }
+
+  return query(part.parent!)
+}
+
+function redraw_layer(
+  ctx: CanvasRenderingContext2D,
+  cb: (ctx: CanvasRenderingContext2D) => void
+) {
+  ctx.clearRect(0, 0, main.width, main.height)
+  ctx.putImageData(blank, main.width, main.height)
+  cb(ctx)
+  compose()
+}
+
+function compose() {
+  // scene.clearRect(0, 0, main.width, main.height)
+  layers.forEach((layer) => scene.drawImage(layer, 0, 0))
+}
---
+++ src/utils.ts
@@ -0,0 +1,42 @@
+export type Coord = { x: number; y: number }
+
+export type Part = {
+  θ: number
+  magnitude: number
+  parent: Part | null
+  x?: number
+  y?: number
+  end_x?: number
+  end_y?: number
+}
+
+export const part = (p: Part) => p
+
+export const convert = (θ: number) => ((360 - θ) % 360) * (Math.PI / 180)
+
+export function compute_end({ x, y }: Coord, θ: number, offset: number): Coord {
+  return {
+    x: x + offset * Math.cos(convert(θ)),
+    y: y + offset * Math.sin(convert(θ)),
+  }
+}
+
+export const make = (n: number) => ({
+  of: <T>(v: T) => Array.from({ length: n }, () => v),
+})
+
+export const fill_frames = (Δ: number, f: number) => make(f).of(Δ / f)
+
+export function clone(canvas: HTMLCanvasElement) {
+  const layer = document.createElement('canvas')
+
+  layer.width = canvas.width
+  layer.height = canvas.height
+
+  return [layer, layer.getContext('2d')!] as [
+    HTMLCanvasElement,
+    CanvasRenderingContext2D
+  ]
+}
+
+export const log = (...args: any) => console.log(...args)
